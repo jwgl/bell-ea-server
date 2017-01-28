ef4a0dae6b11f257f8aa8d2ad0a6f71d58b99ea9
Yang Lin <todoubaba@gmail.com>
2017-01-28 14:38:43 +0800

    Upgrade to grails 3.2.3 (#8)

--- ea-api/grails-app/init/cn/edu/bnuz/bell/ea/api/Application.groovy
+++ ea-api/grails-app/init/cn/edu/bnuz/bell/ea/api/Application.groovy
@@ -1,10 +1,18 @@
 package cn.edu.bnuz.bell.ea.api
 
+import cn.edu.bnuz.bell.config.ExternalConfigLoader
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import org.springframework.context.EnvironmentAware
+import org.springframework.core.env.Environment
 
-class Application extends GrailsAutoConfiguration {
+class Application extends GrailsAutoConfiguration implements EnvironmentAware {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
+    }
+
+    @Override
+    void setEnvironment(Environment environment) {
+        ExternalConfigLoader.load(environment)
     }
 }
